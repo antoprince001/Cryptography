2d93b7c3763c82188730a070e17133a836b9156d
Antony Prince <47826916+antoprince001@users.noreply.github.com>
2022-01-04 18:39:31 +0530

    Create Des.java

---
+++ Des.java
@@ -0,0 +1,33 @@
+import javax.crypto.*;
+import java.util.Base64;
+
+public class Main {
+    public static void main(String[] argv) {
+        try {
+            System.out.println("Message Encryption Using DES Algorithm\n-------");
+            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+            SecretKey myDesKey = keygenerator.generateKey();
+            
+            Cipher desCipher;
+            desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+            
+            byte[] text = "Secret  ".getBytes();
+            System.out.println("Message [Byte Format] : " + text);
+            System.out.println("Message : " + text);
+            
+            byte[] textEncrypted = desCipher.doFinal(text);
+            Base64.Encoder encoder = Base64.getEncoder();
+            String encryptedText = encoder.encodeToString(text);
+            System.out.println("Encrypted Message: " + encryptedText);
+
+            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+            byte[] textDecrypted = desCipher.doFinal(textEncrypted);
+            System.out.println("Decrypted Message: " + new String(textDecrypted));
+        
+        } catch (Exception e) {//U2VjcmV0IEluZm9ybWF0aW9u
+            e.printStackTrace();
+        } 
+           
+    }
+}
